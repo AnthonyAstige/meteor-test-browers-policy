1d73b5ee1629d39aeb734c3bb1525d87dd633729
Anthony Astige <anthonyastige@gmail.com>
2017-05-04 09:21:31 -0500

    Test proof of issue

--- client/main.js
+++ client/main.js
@@ -1,6 +1,19 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+// Follow results in `undefined` being logged to console
+import { BrowserPolicy } from 'meteor/browser-policy';
+console.log(BrowserPolicy);
+
+// Following results in Cannot find module 'meteor/browser-policy-common'
+// import { BrowserPolicy as BrowserPolicyCommon } from 'meteor/browser-policy-common';
+
+// Following results in Cannot find module 'meteor/browser-policy-content'
+// import { BrowserPolicy as BrowserPolicyContent } from 'meteor/browser-policy-content';
+
+// Following results in Cannot find module 'meteor/browser-policy-framing'
+// import { BrowserPolicy as BrowserPolicyFraming } from 'meteor/browser-policy-framing';
+
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
